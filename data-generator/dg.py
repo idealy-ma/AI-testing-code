617cc5aa7ceffc195ab443f12ff562761fe28690
idealy-ma <r.idealy.ma@gmail.com>
2023-06-09 16:40:52 +0300

    Create dg.py

---
+++ data-generator/dg.py
@@ -0,0 +1,17 @@
+import random
+
+def generer_mot():
+    longueur = random.randint(1, 7)
+    
+    mot = ''.join(random.choice('01') for _ in range(longueur))
+    return mot
+
+def generer_langage():
+    nb_mots = random.randint(1, 5)
+    langage = []
+    for _ in range(nb_mots):
+        langage.append(generer_mot())
+    return langage
+
+
+print(generer_langage())
